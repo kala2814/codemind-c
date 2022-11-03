cf9a9be2add0d4f108e44a4b1f4de51f46107a20
kala2814 <111487860+kala2814@users.noreply.github.com>
2022-11-03 23:56:10 +0530

    #include<stdio.h> #include<string.h> int main(){     char w[20];     int x=0,y=0;     scanf("%s",&w);     for(int i=0;w[i]!=NULL;i++){         if(w[i]=='z'){             x++;         }         if(w[i]=='o'){             y++;         }     }     if(y==x*2){         printf("Yes");     }     else{         printf("No");     }     return 0; }

---
+++ Zoos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char w[20];
+    int x=0,y=0;
+    scanf("%s",&w);
+    for(int i=0;w[i]!=NULL;i++){
+        if(w[i]=='z'){
+            x++;
+        }
+        if(w[i]=='o'){
+            y++;
+        }
+    }
+    if(y==x*2){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
